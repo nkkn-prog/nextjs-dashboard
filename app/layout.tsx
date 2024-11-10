911b0bb4884fbb119e1441eab6cf7994f63cc774
Naoki Ando <nkkn@NaokinoMacBook-Pro.local>
2024-11-10 19:22:38 +0900

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,7 @@
+import '@/app/ui/global.css';
+import { noto } from '@/app/ui/fonts';
+import { inter } from '@/app/ui/fonts';
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +9,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${noto.className} antialiased`}>{children}</body>
     </html>
   );
 }
